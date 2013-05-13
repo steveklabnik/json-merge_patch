9be123744fd8b56685b1c1bdf7aaba5078183caa
Steve Klabnik <steve@steveklabnik.com>
2013-05-13 15:41:07 +0100

    Initial commit

---
+++ lib/json/merge_patch.rb
@@ -0,0 +1,7 @@
+require "json/merge_patch/version"
+
+module Json
+  module MergePatch
+    # Your code goes here...
+  end
+end
---
+++ lib/json/merge_patch/version.rb
@@ -0,0 +1,5 @@
+module Json
+  module MergePatch
+    VERSION = "0.0.1"
+  end
+end
