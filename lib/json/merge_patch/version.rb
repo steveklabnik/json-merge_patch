074bcfb3ba0cfd6c8232f721f1ada93cb586d504
Steve Klabnik <steve@steveklabnik.com>
2013-05-18 14:37:36 -0700

    add a small amount of extra docs.
    Now we're at 100%.

--- lib/json/merge_patch/version.rb
+++ lib/json/merge_patch/version.rb
@@ -1,5 +1,8 @@
+# The Ruby standard Library exports JSON
+# http://www.ruby-doc.org/stdlib-1.9.3/libdoc/json/rdoc/
 module JSON
   class MergePatch
+    # The current version of json-merge_patch
     VERSION = "0.1.0"
   end
 end
